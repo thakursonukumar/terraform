d3f7d59262890e72d169d31e9eb7589c57466b0e
Sonukumar Thakur <sonukumar.thakur@ukheshe.com>
2024-12-14 19:55:19 +0530

    update

--- jenkins-container-buildAgent/main.tf
+++ jenkins-container-buildAgent/main.tf
@@ -69,5 +69,5 @@ resource "aws_instance" "jenkins-build-agent" {
 
 
 output "public-ip-ec2" {
-  value = aws_instance.jenkins-build-agent[0]
+  value = aws_instance.jenkins-build-agent[0].public_ip
 }
